f8c913d8a9987d5f3b13d455a4f5a448e1af1736
TheoSrb- <143961678+TheoSrb@users.noreply.github.com>
2026-02-28 17:12:45 +0100

    Add files via upload

---
+++ Portfolio/script/globals.js
@@ -0,0 +1,15 @@
+const NAME = "SARBACH-FISCHER";
+const FIRST_NAME = "Théo";
+const AGE = 19;
+
+const SCHOOL = "Lycée Polyvalent René Cassin";
+const SCHOOL_LOCATION = "Strasbourg";
+const SCHOOL_COUNTRY = "France";
+
+const STUDIES_LICENSE = "BTS SIO";
+const STUDIES_LEVEL = "2ème année";
+const LICENSES_LEVEL = "Bac +2";
+
+const ABOUT_TEXT = `Je m'appelle <b>${FIRST_NAME} ${NAME}</b>, j'ai <b><span id="age"></span></b> ans et je poursuis actuellement mes études au lycée <b>${SCHOOL}</b> 
+    à <b>${SCHOOL_LOCATION}</b>, dans le cadre du BTS Services Informatiques aux Organisations (<b>SIO</b>), option Solutions Logicielles et Applications Métiers (<b>SLAM</b>). Je me 
+    spécialise particulièrement dans la programmation et le développement d'applications.`;
